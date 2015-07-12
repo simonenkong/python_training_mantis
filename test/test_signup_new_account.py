4626f3ae6461d76c35bcdc9643a8751514472c5e
simonenkong <simonenkong@yandex.ru>
2015-07-12 17:20:55 +0300

    SOAP added

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -3,9 +3,11 @@ __author__ = 'Nataly'
 import string
 import random
 
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
 
 def test_signup_nem_account(app):
     username = random_string("user_", 10)
@@ -13,6 +15,4 @@ def test_signup_nem_account(app):
     password = "test"
     app.james.ensure_user_exists(username, password)
     app.signup.new_user(username, email, password)
-    app.session.login(username, password)
-    assert app.session.is_logged_in_as(username)
-    app.session.logout()
+    assert app.soap.can_login(username, password)
