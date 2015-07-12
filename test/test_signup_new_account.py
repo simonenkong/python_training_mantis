f8c0d0850ea435b8a637fa9848a1ab1f5c9a016b
simonenkong <simonenkong@yandex.ru>
2015-07-12 16:59:24 +0300

    Pop3 mail-checking added

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,7 +1,18 @@
 __author__ = 'Nataly'
 
+import string
+import random
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def test_signup_nem_account(app):
-    username = "user1"
+    username = random_string("user_", 10)
+    email = username + "@localhost"
     password = "test"
     app.james.ensure_user_exists(username, password)
+    app.signup.new_user(username, email, password)
+    app.session.login(username, password)
+    assert app.session.is_logged_in_as(username)
+    app.session.logout()
